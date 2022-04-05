61749c02462da3da02186fdb095d3360f8590b43
Eric <marquezeric@hotmail.com>
2022-04-05 13:39:18 -0500

    Add files via upload

---
+++ config/params.php
@@ -0,0 +1,12 @@
+<?php
+
+/*return [
+    'adminEmail' => 'admin@example.com',
+];
+
+<?php*/
+return[// Lineas agreagadas para el uso del Login
+	'adminEmail' => 'emarquez@bcb.com.mx',// Lineas agreagadas para el uso del Login
+	'title' => 'Aplicación Yii',// Lineas agreagadas para el uso del Login
+	'salt' => 'fsddsflj38343lj0',// Lineas agreagadas para el uso del Login
+];
